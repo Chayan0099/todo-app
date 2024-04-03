e391b365bde60bd7bb9542adad1b05329c845c50
Chayan Sarkar <131506479+Chayan0099@users.noreply.github.com>
2024-04-03 23:33:12 +0530

    creating components folder

---
+++ frontend/src/components/Createtodo.jsx
@@ -0,0 +1,9 @@
+export function Createtodo(){
+    return ( <div>
+        <input type="text" placeholder="title" ></input><br />
+        <input type="text" placeholder="description"></input><br />
+
+        <button>Add todo</button>
+        </div>
+    )
+}
